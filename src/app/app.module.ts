63ebf74a9c55f1c9398fb0ce96dac2752a12ad20
queren sales <querenhsales@gmail.com>
2022-01-29 22:11:31 -0300

    curso Loiane

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
+import { SegundoComponentComponent } from './segundo-component/segundo-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponentComponent,
+    SegundoComponentComponent
   ],
   imports: [
     BrowserModule,
